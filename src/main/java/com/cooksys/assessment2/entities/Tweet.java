47b93ed6fd7c330bf170b86c71b55bb2a94794b0
Artur Kaminski <arturxkaminski@gmail.com>
2021-10-18 18:50:35 -0400

    Entities Finished

--- src/main/java/com/cooksys/assessment2/entities/Tweet.java
+++ src/main/java/com/cooksys/assessment2/entities/Tweet.java
@@ -1,13 +1,9 @@
 package com.cooksys.assessment2.entities;
 
+import java.sql.Timestamp;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,14 +11,53 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @Data
-
 public class Tweet {
 
     @Id
     @GeneratedValue
-
     private Long id;
 
-    private String text;
+    @Column(nullable = false)
+    @ManyToOne
+    @JoinColumn
+    private Users author;
+
+    @Column(nullable = false)
+    private Timestamp posted;
+
+    @Column(nullable = false)
+    private boolean deleted;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    @OneToMany(mappedBy = "replies")
+    private Tweet inReplyTo;
+
+    @Column(nullable = false)
+    @ManyToOne
+    @JoinColumn
+    private List<Tweet> replies;
+
+    @Column(nullable = false)
+    @OneToMany(mappedBy = "reposts")
+    private Tweet repostOf;
+
+    @Column(nullable = false)
+    @ManyToOne
+    @JoinColumn
+    private List<Tweet> reposts;
+
+    @Column(nullable = false)
+    private List<Users> likes;
+
+    @Column(nullable = false)
+    private List<Hashtag> hashtags;
+
+    @Column(nullable = false)
+    @ManyToMany(mappedBy = "tweets")
+    private List<Users> usersMentioned;
+
 
 }
--- src/main/java/com/cooksys/assessment2/entities/Users.java
+++ src/main/java/com/cooksys/assessment2/entities/Users.java
@@ -1,13 +1,9 @@
 package com.cooksys.assessment2.entities;
 
+import java.sql.Timestamp;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,14 +11,52 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @Data
-
 public class Users {
 
     @Id
     @GeneratedValue
-
     private Long id;
 
-    private String text;
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Timestamp joined;
+
+    @Column(nullable = false)
+    private boolean deleted;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+
+    @Column(nullable = false)
+    @OneToMany(mappedBy = "author")
+    private List<Tweet> tweets;
+
+    @Column(nullable = false)
+    private List<Tweet> likedTweets;
+
+    @Column(nullable = false)
+    @ManyToMany(mappedBy = "followers")
+    private List<Users> following;
+
+    @Column(nullable = false)
+    @ManyToMany(mappedBy = "following")
+    private List<Users> followers;
+
+    @Column(nullable = false)
+    private List<Tweet> mentions;
+
 
 }
