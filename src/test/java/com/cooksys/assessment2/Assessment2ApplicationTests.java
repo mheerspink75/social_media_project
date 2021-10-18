62d0dc554048bcdc1eff60ae42d6d9b530002dcf
Artur Kaminski <arturxkaminski@gmail.com>
2021-10-18 18:09:56 -0400

    Initial Commit, Project Base

---
+++ src/test/java/com/cooksys/assessment2/Assessment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooksys.assessment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assessment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
