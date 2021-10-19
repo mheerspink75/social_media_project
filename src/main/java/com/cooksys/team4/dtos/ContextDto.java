d3b06a5856fd6f40fc0999da50cc3b2063ba4f09
Artur Kaminski <arturxkaminski@gmail.com>
2021-10-19 13:05:09 -0400

    Request + Response DTO fields populated

--- src/main/java/com/cooksys/team4/dtos/ContextDto.java
+++ src/main/java/com/cooksys/team4/dtos/ContextDto.java
@@ -1,5 +1,6 @@
 package com.cooksys.team4.dtos;
 
+import com.cooksys.team4.entities.Tweet;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,7 +8,13 @@ import java.util.List;
 
 @NoArgsConstructor
 @Data
-
 public class ContextDto {
+
+    private Tweet target;
+
+    private List<TweetResponseDto> before;
+
+    private List<TweetResponseDto> after;
+
 
 }
