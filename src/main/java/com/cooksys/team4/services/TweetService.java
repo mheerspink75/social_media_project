c658a24b3ef3cb9ed582df86a62ab200b72342a6
Baktygul <baliev@my.stlcc.edu>
2021-10-19 14:03:36 -0500

    Update TweetService.java

--- src/main/java/com/cooksys/team4/services/TweetService.java
+++ src/main/java/com/cooksys/team4/services/TweetService.java
@@ -4,5 +4,18 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 
+import com.cooksys.team4.dtos.CredentialsDto;
+import com.cooksys.team4.dtos.TweetResponseDto;
+import com.cooksys.team4.entities.Tweet;
+
 public interface TweetService {
+	
+	public List<TweetResponseDto> getTweets();
+	
+	public TweetResponseDto postTweet(CredentialsDto credentialsDto, String content);
+	
+	public TweetResponseDto getTweetById(Long id);
+	
+	public TweetResponseDto deleteTweetById(Long id);
+	
 }
