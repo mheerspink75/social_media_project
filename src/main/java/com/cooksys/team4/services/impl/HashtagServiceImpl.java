fea18be82e82e13c19e0de91bc1b4677b50a493a
Artur Kaminski <arturxkaminski@gmail.com>
2021-10-19 16:18:27 -0400

    Clean up

--- src/main/java/com/cooksys/team4/services/impl/HashtagServiceImpl.java
+++ src/main/java/com/cooksys/team4/services/impl/HashtagServiceImpl.java
@@ -6,8 +6,6 @@ import com.cooksys.team4.dtos.HashTagDto;
 import com.cooksys.team4.entities.Hashtag;
 import com.cooksys.team4.entities.Tweet;
 import com.cooksys.team4.services.HashTagService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -15,7 +13,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class HashtagServiceImpl implements HashTagService {
-    
+
     @Override
     public List<HashTagDto> getAllHashtags() {
         return null;
