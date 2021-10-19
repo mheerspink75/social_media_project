2c49a25e480f1accd2a6a6bfc0b31db682173f00
Matt Heerspink <mheerspink75@gmail.com>
2021-10-19 14:20:43 -0500

    updated HashtagController and ValidateController

--- src/main/java/com/cooksys/team4/controllers/ValidateController.java
+++ src/main/java/com/cooksys/team4/controllers/ValidateController.java
@@ -8,11 +8,44 @@ import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.web.bind.annotation.RestController;
+
 
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/")
+@RequestMapping("/validate")
 
 public class ValidateController {
+
+    @GetMapping("username/exists/@{username}")
+    public void userNameExists(@PathVariable String username) {
+        /**
+         * TODO: implement
+         * Get /username/exists/@{username}
+         * Checks whether or not a given username exists.
+         * Response: 'boolean'
+         */
+    }
+
+    @GetMapping("username/available/@{username}")
+    public void userNameAvailable(@PathVariable String username) {
+
+        /**
+         * TODO: implement
+         * Get /username/available/@{username}
+         * Checks whether or not a given username is available.
+         * Response: 'boolean'
+         */
+    }
+
+    @GetMapping("tag/exists/@{label}")
+    public void tagExists(@PathVariable String label) {
+        /**
+         * TODO: implement
+         * Get /tag/exists/@{label}
+         * Checks whether or not a given hashtag exists.
+         * Response: 'boolean'
+         */
+    }
 }
