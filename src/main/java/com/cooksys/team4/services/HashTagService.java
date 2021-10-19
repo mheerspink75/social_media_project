aade7b4471e36b012cc89657f905f8d454aac056
Art K <65674238+DevArtK@users.noreply.github.com>
2021-10-19 18:22:49 -0400

    add hashtag service

--- src/main/java/com/cooksys/team4/services/HashTagService.java
+++ src/main/java/com/cooksys/team4/services/HashTagService.java
@@ -1,4 +1,22 @@
 package com.cooksys.team4.services;
 
+import com.cooksys.team4.dtos.HashTagDto;
+import com.cooksys.team4.entities.Hashtag;
+import com.cooksys.team4.entities.Tweet;
+
+import java.util.List;
+
 public interface HashTagService {
+
+    /**
+     * Retrieves all hashtags tracked by the database
+     */
+    public List<HashTagDto> getAllHashtags();
+
+    /**
+     * Retrieves all (non-deleted) tweets tagged with the given hashtag label. The tweets should appear in reverse-chronological order. If no hashtag with the given label exists, an error should be sent in lieu of a response.
+       A tweet is considered "tagged" by a hashtag if the tweet has content and the hashtag's label appears in that content following a #
+     */
+    public List<Tweet> getTweetsByHashtagLabel(Hashtag label);
+
 }
