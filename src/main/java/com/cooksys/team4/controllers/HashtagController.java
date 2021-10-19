ccc924441e9ef3e6cfe6ca428a85168fabbae98b
Matt Heerspink <mheerspink75@gmail.com>
2021-10-19 11:53:39 -0500

    added controller advice and custom error handliing, updated ErrorDto, updated exceptions

--- src/main/java/com/cooksys/team4/controllers/HashtagController.java
+++ src/main/java/com/cooksys/team4/controllers/HashtagController.java
@@ -3,7 +3,12 @@ package com.cooksys.team4.controllers;
 
 import java.util.List;
 
+import com.cooksys.team4.services.HashTagService;
+import com.cooksys.team4.dtos.HashTagDto;
+import com.cooksys.team4.dtos.ErrorDto;
 
+
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,9 +17,12 @@ import lombok.RequiredArgsConstructor;
 
 
 @RestController
-@RequiredArgsConstructor
-@RequestMapping("/")
-
+@AllArgsConstructor
+@RequestMapping("hashtags")
 
 public class HashtagController {
+
+   // private HashTagService hashTagService;
+
+
 }
