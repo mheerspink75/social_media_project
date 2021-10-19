cbce70342f3e056003fc20275b7e19d651003d01
Matt Heerspink <mheerspink75@gmail.com>
2021-10-18 23:52:43 -0500

    updated controller imports

--- src/main/java/com/cooksys/team4/controllers/HashtagController.java
+++ src/main/java/com/cooksys/team4/controllers/HashtagController.java
@@ -1,4 +1,20 @@
 package com.cooksys.team4.controllers;
 
+
+import java.util.List;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.RequiredArgsConstructor;
+
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
+
+
 public class HashtagController {
 }
--- src/main/java/com/cooksys/team4/controllers/UserController.java
+++ src/main/java/com/cooksys/team4/controllers/UserController.java
@@ -1,4 +1,18 @@
 package com.cooksys.team4.controllers;
 
+import java.util.List;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.RequiredArgsConstructor;
+
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
+
 public class UserController {
 }
