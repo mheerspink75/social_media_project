4a6a896939fea5f3e5e2f92513bc13e92eebb2ea
Artur Kaminski <arturxkaminski@gmail.com>
2021-10-19 16:15:10 -0400

    HashtagService + Implementing class started

--- src/main/java/com/cooksys/team4/services/impl/HashtagServiceImpl.java
+++ src/main/java/com/cooksys/team4/services/impl/HashtagServiceImpl.java
@@ -2,6 +2,10 @@ package com.cooksys.team4.services.impl;
 
 import java.util.List;
 
+import com.cooksys.team4.dtos.HashTagDto;
+import com.cooksys.team4.entities.Hashtag;
+import com.cooksys.team4.entities.Tweet;
+import com.cooksys.team4.services.HashTagService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,5 +14,15 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class HashtagServiceImpl {
+public class HashtagServiceImpl implements HashTagService {
+    
+    @Override
+    public List<HashTagDto> getAllHashtags() {
+        return null;
+    }
+
+    @Override
+    public List<Tweet> getTweetsByHashtagLabel(Hashtag label) {
+        return null;
+    }
 }
