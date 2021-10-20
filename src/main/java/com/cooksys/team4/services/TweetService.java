61e37b0011a90695476de7035c6aa0ccae470ceb
Baktygul <baliev@my.stlcc.edu>
2021-10-20 11:07:53 -0500

    completed task 38

--- src/main/java/com/cooksys/team4/services/TweetService.java
+++ src/main/java/com/cooksys/team4/services/TweetService.java
@@ -1,10 +1,13 @@
-package com.cooksys.tVeam4.services;
+package com.cooksys.team4.services;
 
 import com.cooksys.team4.dtos.TweetResponseDto;
+import com.cooksys.team4.dtos.UserResponseDto;
 
 import java.util.List;
 
+import com.cooksys.team4.dtos.ContextDto;
 import com.cooksys.team4.dtos.CredentialsDto;
+import com.cooksys.team4.dtos.HashTagDto;
 import com.cooksys.team4.dtos.TweetResponseDto;
 import com.cooksys.team4.entities.Tweet;
 
@@ -17,5 +20,25 @@ public interface TweetService {
 	public TweetResponseDto getTweetById(Long id);
 	
 	public TweetResponseDto deleteTweetById(Long id);
+	
+	public void likeTweet(Long id, CredentialsDto credentialsDto);
+	
+	public TweetResponseDto replyTweet(Long id, CredentialsDto credentialsDto, String content);
+	
+	public TweetResponseDto repostTweet(Long id, CredentialsDto credentialsDto);
+	
+	public List<HashTagDto> getHashTags(Long id);
+	
+	public List<UserResponseDto> getLikes(Long id);
+	
+	public ContextDto getContext(Long id);
+	
+	public List<TweetResponseDto> getReplies(Long id);
+	
+	public List<TweetResponseDto> getReposts(Long id);
+	
+	public List<UserResponseDto> getMentions(Long id); 
+	
+	
 	
 }
