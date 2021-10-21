b6a7b3b1d366835c2a4fe73a7ee744387771a4aa
Artur Kaminski <arturxkaminski@gmail.com>
2021-10-20 21:58:15 -0400

    Changed BadRequestException's where checking for user existance to NotFoundExceptions

--- src/main/java/com/cooksys/team4/services/impl/UserServiceImpl.java
+++ src/main/java/com/cooksys/team4/services/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import com.cooksys.team4.dtos.UserResponseDto;
 import com.cooksys.team4.entities.Credentials;
 import com.cooksys.team4.entities.User;
 import com.cooksys.team4.exceptions.BadRequestException;
+import com.cooksys.team4.exceptions.NotFoundException;
 import com.cooksys.team4.mappers.UserMapper;
 import com.cooksys.team4.repositories.UserRepository;
 import com.cooksys.team4.services.UserService;
@@ -101,11 +102,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserResponseDto getUser(String username) {
         Optional<User> optionalUser = userRepository.findByCredentialsUsername(username);
-        User existingUser = optionalUser.orElseThrow(() -> new BadRequestException(
+        User existingUser = optionalUser.orElseThrow(() -> new NotFoundException(
                 "User doesn't exist"));
 
         if (existingUser.isDeleted()) {
-            throw new BadRequestException("Unable to get user");
+            throw new NotFoundException("Unable to get user");
         }
 
         return userMapper.entityToResponseDto(existingUser);
